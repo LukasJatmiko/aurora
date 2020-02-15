73c5b31126abd00d71cb6494da1e3a557656a15b
Lukas Jatmiko <lucasjatmiko@gmail.com>
2020-02-15 21:53:56 +0700

    allow other format (not only string)

--- aurora.go
+++ aurora.go
@@ -2,6 +2,7 @@ package aurora
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"regexp"
@@ -54,25 +55,31 @@ func (aurora *Aurora) Render(templateName string, datas map[string]interface{}) 
 
 	//render loops
 	rgx := regexp.MustCompile(`(?is)(\{\{\s*for\.([a-z]*)\.in\.([a-z]*))(.*?)(endfor\s*\}\})`)
+	var loopVars []interface{}
 	for _, loop := range rgx.FindAllSubmatch(aurora.Templates[templateName].Data, -1) {
 		aurora.Templates[templateName].Data = bytes.Replace(aurora.Templates[templateName].Data, loop[1], []byte(""), 1)
 		aurora.Templates[templateName].Data = bytes.Replace(aurora.Templates[templateName].Data, loop[5], []byte(""), 1)
 
 		re := regexp.MustCompile(`(?is)\{\{\s*` + string(loop[2]) + `\s*\}\}`)
 		loopstr := ""
-		for _, item := range datas[string(loop[3])].([]string) {
+		for _, item := range datas[string(loop[3])].([]interface{}) {
 			temp := loop[4]
-			temp = re.ReplaceAllLiteral([]byte(temp), []byte(item))
+			temp = re.ReplaceAllLiteral([]byte(temp), []byte("%v"))
 			loopstr += string(temp)
+			loopVars = append(loopVars, item)
 		}
 
 		aurora.Templates[templateName].Data = bytes.Replace(aurora.Templates[templateName].Data, loop[4], []byte(loopstr), 1)
 	}
+	aurora.Templates[templateName].Data = []byte(fmt.Sprintf(string(aurora.Templates[templateName].Data), loopVars...))
 
+	loopVars = nil
 	rgx = regexp.MustCompile(`\{\{\s*([a-z]*)\s*\}\}`)
 	for _, param := range rgx.FindAllSubmatch(aurora.Templates[templateName].Data, -1) {
-		aurora.Templates[templateName].Data = bytes.Replace(aurora.Templates[templateName].Data, param[0], []byte(datas[string(param[1])].(string)), 1)
+		loopVars = append(loopVars, datas[string(param[1])])
+		aurora.Templates[templateName].Data = bytes.Replace(aurora.Templates[templateName].Data, param[0], []byte("%v"), 1)
 	}
+	aurora.Templates[templateName].Data = []byte(fmt.Sprintf(string(aurora.Templates[templateName].Data), loopVars...))
 
 	return aurora.Templates[templateName].Data
 }
